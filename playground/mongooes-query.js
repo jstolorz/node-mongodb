1ba60be3241383c1a5b572a243c8d661e9e01a4a
jstolorz <Jmnz1234>
2018-06-25 14:11:21 +0200

    Adding some test blocks

---
+++ playground/mongooes-query.js
@@ -0,0 +1,32 @@
+const {mongoose} = require('../server/db/mongoose');
+const {ListaZadan} = require('../server/models/todo');
+const {User} = require('../server/models/user');
+
+let id = '5b30d93396affd46aae63bd6';
+
+ListaZadan.find({
+    _id: id
+}).then((todos)=>{
+    console.log(todos);
+});
+
+ListaZadan.findOne({
+    _id: id
+}).then((todos)=>{
+   console.log(todos);
+});
+
+ListaZadan.findById(id).then((todo)=>{
+    if(!todo){
+      return console.log('Id not found');
+    }
+
+    console.log(todo);
+
+}).catch((e) => console.log(e));
+
+
+User.findById('5b2bb1f306f9db4d237ebd3b').then((user)=>{
+
+
+});
--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,9 +4,19 @@ const request = require('supertest');
 const {app} = require('./../server');
 const {ListaZadan} = require('./../models/todo');
 
-beforeEach((done) => {
-    ListaZadan.remove({}).then(() => done());
-});
+const todos = [{
+    text: 'Nakarmić pasa'
+},{
+    text: 'Trening kettelball'
+}];
+
+// beforeEach((done) => {
+//     ListaZadan.remove({})
+//         .then(()=>{
+//            return ListaZadan.insertMany(todos);
+//         })
+//         .then(() => done());
+// });
 
 describe('POST /todos', () =>{
 
@@ -18,16 +28,48 @@ describe('POST /todos', () =>{
             .post('/todos')
             .send({text})
             .expect(200)
-            .end((err,res) =>{
-
+            .end((err,res)=>{
                 if(err){
-                    return(done(err));
+                    return done(err);
                 }
 
-                done();
+                ListaZadan.find({text}).then((todos)=>{
+                    expect(todos.length).toBe(1);
+                    expect(todos[0].text).toBe(text);
+                    done();
+                }).catch((e) => done(e));
+            });
+    });
 
-            }).catch((e) => done(e));
+    it('should not create todos with invalid body data', (done) => {
+        request(app)
+            .post('/todos')
+            .send({})
+            .expect(400)
+            .end((err,res) =>{
+               if(err){
+                   return done(err);
+               }
 
+               ListaZadan.find().then((todos)=>{
+                   expect(todos.length).toBe(2);
+                   done();
+               }).catch((e)=>done(e));
+
+            });
+    });
+});
+
+describe('GET /todos', ()=>{
+
+    it('should get all todos', (done) => {
+       request(app)
+           .get('/todos')
+           .expect(200)
+           .expect((res)=>{
+               expect(res.body.todos.length).toBe(2);
+           })
+           .end(done);
     });
 
 });
