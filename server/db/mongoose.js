f33039d4a1d800880a5750c93efd385a8d08decf
jstolorz <Jmnz1234>
2018-06-26 15:01:00 +0200

    reconfiguration mongodb

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,6 +2,6 @@ let mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 //mongoose.connect('mongodb://localhost:27017/TodoApp');
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.MONGOLAB_CRIMSON_URI || 'mongodb://localhost:27017/TodoApp');
 
 module.exports = {mongoose};
