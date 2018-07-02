5bc5c63c1977a87d1a533027b3a4a14cd9e93315
jstolorz <Jmnz1234>
2018-07-02 13:26:29 +0200

    Adding bcript for password

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,13 +1,26 @@
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-let data = {
-    id: 10
-};
+let password = '123abc!';
+
+bcrypt.genSalt(10, (err, salt) => {
+    console.log(salt);
+
+    bcrypt.hash(password, salt, (err, hash) => {
+        console.log(hash);
+    });
+
+});
 
 
-let token = jwt.sign(data, 'olobolo');
-console.log(token);
-
-let decode = jwt.verify(token, 'olobolo');
-console.log(decode);
-
+// let data = {
+//     id: 10
+// };
+//
+//
+// let token = jwt.sign(data, 'olobolo');
+// console.log(token);
+//
+// let decode = jwt.verify(token, 'olobolo');
+// console.log(decode);
+//
