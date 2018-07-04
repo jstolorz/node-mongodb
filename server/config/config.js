b48e94d06d148896bb4f2027ac2f0b672d6668e1
jstolorz <Jmnz1234>
2018-07-04 15:35:32 +0200

    test gitignore

--- server/config/config.js
+++ server/config/config.js
@@ -1,9 +1,19 @@
 let env = process.env.NODE_ENV || 'development';
 
-if(env === 'development'){
-    process.env.PORT = 3000;
-    process.env.MONGOLAB_CRIMSON_URI = 'mongodb://localhost:27017/TodoApp';
-}else if(env === 'test'){
-    process.env.PORT = 3000;
-    process.env.MONGOLAB_CRIMSON_URI = 'mongodb://localhost:27017/TodoAppTest';
+if(env === 'development' || env === 'test'){
+
+    let config = require('./config.json');
+    let envConfig = config[env];
+
+    console.log(envConfig);
+
 }
+
+
+// if(env === 'development'){
+//     process.env.PORT = 3000;
+//     process.env.MONGOLAB_CRIMSON_URI = 'mongodb://localhost:27017/TodoApp';
+// }else if(env === 'test'){
+//     process.env.PORT = 3000;
+//     process.env.MONGOLAB_CRIMSON_URI = 'mongodb://localhost:27017/TodoAppTest';
+// }
