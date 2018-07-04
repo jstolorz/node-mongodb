caf7501c55cba8e69733a3511d989bff18502e10
jstolorz <Jmnz1234>
2018-07-04 15:40:53 +0200

    test gitignore 4

--- server/config/config.js
+++ server/config/config.js
@@ -5,7 +5,9 @@ if(env === 'development' || env === 'test'){
     let config = require('./config.json');
     let envConfig = config[env];
 
-    console.log(envConfig);
+    Object.keys(envConfig).forEach((key) => {
+        process.env[key] = envConfig[key];
+    });
 
 }
 
